d64ba4c647cd70a3e3894585b841cc4b33d84c9f
GI-Te <you@example.com>
2021-12-15 02:58:17 -0800

    userswebservices

---
+++ README.md
@@ -0,0 +1,3 @@
+# UsersWebservices
+
+this is a web service that is used to manage users. It is allows creation and fatching of users.
---
+++ data/users.js
@@ -0,0 +1,6 @@
+let users = [
+    {fname:"Adme",lname:"Kin",dob:"15/06/1998",school:"Wa poly"},
+    {fname:"Felicity",lname:"Brown",dob:"04/1/1994",school:"Tamale poly"},
+    {fname:"Ginger",lname:"Simon",dob:"01/08/2000",school:"KNUST"},
+]
+module.exports =users;
